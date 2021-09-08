8f1aabafbbd6a4a87be6a9c76d89bc93d6dea5ff
Wesley Westelley <wesleywestelley@gmail.com>
2021-09-08 17:25:55 -0300

    :wrench: feat: created routers for priority and contents
    router for change priority and contents (notes).

--- backend/src/routers/index.js
+++ backend/src/routers/index.js
@@ -1,9 +1,16 @@
 import express from "express";
 import AnnotationController from "../controllers/AnnotationController.js";
+import PriorityController from "../controllers/PriorityController.js";
+import ContentController from "../controllers/ContentController.js";
 const routes = express.Router();
 
 routes.get("/annotations", AnnotationController.read);
 routes.post("/annotations", AnnotationController.create);
 routes.delete("/annotations/:id", AnnotationController.delete);
+
+routes.get("/priorities", PriorityController.read);
+routes.put("/priorities/:id", PriorityController.update);
+
+routes.put("/contents/:id", ContentController.update);
 
 export default routes;
