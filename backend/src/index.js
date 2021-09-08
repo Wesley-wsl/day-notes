a12999c40231b20a9edbb1c539f3334ffda0b40b
Wesley Westelley <wesleywestelley@gmail.com>
2021-09-08 17:24:48 -0300

    :wrench: feat: Added cors in application.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import routes from "./routers/index.js";
+import cors from "cors";
 import "./config/dbConfig.js";
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
