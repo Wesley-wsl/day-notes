bf209d600013297c14c87f70a8fd0f27bc6138cd
Wesley Westelley <wesleywestelley@gmail.com>
2021-09-10 17:44:00 -0300

    :wrench: feat: consuming api to generate notes.
    This code get all notes from backend and renderizing in display.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,34 @@
-import Sidebar from './components/Sidebar';
-import './styles/global.css';
+import Sidebar from "./components/Sidebar";
+import Notes from "./components/Notes";
+import "./styles/global.css";
+import "./styles/app.css";
+import { useEffect, useState } from "react";
+import api from "./services/api.js";
 
 function App() {
-  return (
-    <div id="app">
-        <Sidebar />
-    </div>
-  );
+    const [allNotes, setAllNotes] = useState(Array);
+
+    useEffect(() => {
+        async function getAllNotes() {
+            const response = await api.get("/annotations");
+            setAllNotes(response.data);
+        }
+
+        getAllNotes();
+    }, []);
+
+    return (
+        <div id="app">
+            <Sidebar allNotes={allNotes} setAllNotes={setAllNotes} />
+            <main>
+                <ul>
+                    {allNotes.map((data, index) => (
+                        <Notes data={data} key={index} />
+                    ))}
+                </ul>
+            </main>
+        </div>
+    );
 }
 
 export default App;
